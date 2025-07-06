57b1e7fb293fdbb81c60c832988212cf2e4f0107
Yaroslav Kelemen <yaroslavkelemen14@gmail.com>
2025-07-06 19:34:23 +0300

    Configured EmailJS integration

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -67,29 +67,29 @@ const Contact = () => {
 
     emailjs
       .send(
-        "service_r2i0by4",
-        "template_mf5x3bh",
+        "service_k7wtkar",
+        "template_ju5kiog",
         {
           from_name: form.name,
-          to_name: "Lohit Kolluri",
-          from_email: form.email,
-          to_email: "lohitkolluri@gmail.com",
+          name: "Yaroslav",
+          email: form.email,
+          to_email: "yaroslav.project2@gmail.com",
           message: form.message,
         },
-        "p-gXzzyvEhPaJ0XA-"
+        "ZWovhN2f0KsaYorIJ"
       )
-      .then(
-        () => {
-          setLoading(false);
-          setConfirmation("Thank you! I will get back to you as soon as possible.");
+      .then(() => {
+        setLoading(false);
+        setConfirmation(
+          "Thank you! I will get back to you as soon as possible."
+        );
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        }
-      )
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      })
       .catch((error) => {
         setLoading(false);
         console.error(error);
@@ -98,12 +98,21 @@ const Contact = () => {
   };
 
   return (
-    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
-      <motion.div variants={slideIn("left", "tween", 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact Me</h3>
 
-        <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
           <InputField
             label="Your Name"
             name="name"
@@ -143,7 +152,10 @@ const Contact = () => {
         </form>
       </motion.div>
 
-      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
         <EarthCanvas />
       </motion.div>
     </div>
