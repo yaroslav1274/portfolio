fc9397ede2673854977eae4dc7ccc534e4f48177
Yaroslav Kelemen <yaroslavkelemen14@gmail.com>
2025-07-07 23:56:05 +0300

    Name change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">valsoray</span>
+            Hi, I'm <span className="text-[#915EFF]">Yaroslav</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I do
